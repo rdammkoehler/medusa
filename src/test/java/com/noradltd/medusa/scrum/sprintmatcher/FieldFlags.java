3cac111220b42e137fa89b4f3cf922a1b3c57ba6
Rich Dammkoehler <rpd@noradltd.com>
2015-01-06 14:33:03 -0500

    removed previous implementation in order to create the atomized implementation of the same matcher

--- src/test/java/com/noradltd/medusa/scrum/sprintmatcher/FieldFlags.java
+++
@@ -1,5 +0,0 @@
-package com.noradltd.medusa.scrum.sprintmatcher;
-
-enum FieldFlags {
-	DEVELOPER_IDLE_DAYS_FIELD;
-}
--- src/test/java/com/noradltd/medusa/scrum/sprintmatcher/SprintResultFuzzyMatcher.java
+++ src/test/java/com/noradltd/medusa/scrum/sprintmatcher/SprintResultFuzzyMatcher.java
@@ -1,88 +1,26 @@
 package com.noradltd.medusa.scrum.sprintmatcher;
 
-import java.util.Set;
-
 import org.hamcrest.Description;
 import org.hamcrest.Factory;
 import org.hamcrest.TypeSafeMatcher;
 
-import com.noradltd.medusa.scrum.Card;
 import com.noradltd.medusa.scrum.SprintResult;
 
 public final class SprintResultFuzzyMatcher extends TypeSafeMatcher<SprintResult> {
 
-	final MatchData matchData = new MatchData();
 
 	@Factory
 	public static SprintResultFuzzyMatcher fuzzyMatchesSprintResults() {
-		return new SprintResultFuzzyMatcher();
+		return null; 
 	}
 
 	@Override
 	public void describeTo(Description description) {
-		description.appendText("The given Sprint result does not match the expectation!");
 	}
 
 	@Override
 	protected boolean matchesSafely(SprintResult sprintResult) {
-		return new SprintMatcherSelector().selectMatcherFor(matchData).match(matchData, sprintResult);
-	}
-
-	public SprintResultFuzzyMatcher withOriginalSprintData(String originalSprintData) {
-		matchData.originalSprintData = originalSprintData;
-		return this;
-	}
-
-	public SprintResultFuzzyMatcher withVerifiedCardsMatch(Set<Card> verified) {
-		if (matchData.preventVerifiedListAblation) {
-			System.err.println("You can only modify the Verified Card Set matcher once");
-		} else {
-			matchData.addAllVerified(verified);
-		}
-		return this;
-	}
-
-	public SprintResultFuzzyMatcher whereVerifiedCardsContains(Card verifiedCard) {
-		if (matchData.preventVerifiedListAblation) {
-			System.err.println("You can only modify the Verified Card Set matcher once");
-		} else {
-			matchData.addFuzzySearchFlags(matchData.verified);
-			matchData.addVerified(verifiedCard);
-		}
-		return this;
-	}
-
-	public SprintResultFuzzyMatcher whereVerifiedCardsContains(Set<Card> verifiedCards) {
-		if (matchData.preventVerifiedListAblation) {
-			System.err.println("You can only modify the Verified Card Set matcher once");
-		} else {
-			matchData.addFuzzySearchFlags(matchData.verified);
-			matchData.addAllVerified(verifiedCards);
-		}
-		return this;
-	}
-
-	public SprintResultFuzzyMatcher whereVerifiedCardsDoesNotContain(Card card) {
-		if (matchData.preventVerifiedListAblation) {
-			System.err.println("You can only modify the Verified Card Set matcher once");
-		} else {
-			matchData.addFuzzySearchFlags(matchData.verified);
-			matchData.addDontFindFlags(matchData.verified);
-			matchData.addVerified(card);
-		}
-		return this;
-	}
-
-	public SprintResultFuzzyMatcher whereDeveloperIdleDaysAre(Integer idleDays) {
-		matchData.developerIdleDays = idleDays;
-		return this;
-	}
-
-	public SprintResultFuzzyMatcher whereDeveloperIdleDaysAreBetween(Integer lowerLimit, Integer upperLimit) {
-		matchData.addFuzzyMatch(FieldFlags.DEVELOPER_IDLE_DAYS_FIELD);
-		matchData.minimumDeveloperIdleDays = lowerLimit;
-		matchData.maximumDeveloperIdleDays = upperLimit;
-		return this;
+		return false; 
 	}
 
 }
