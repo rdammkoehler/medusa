1223f386c57f9a5c580d29f292ddeb1c10cf5bca
Rich Dammkoehler <rpd@noradltd.com>
2015-01-06 21:05:44 -0500

    added stuff for most of the other collections onto the matcher, more tests required

--- src/test/java/com/noradltd/medusa/scrum/SprintResultMatcher.java
+++ src/test/java/com/noradltd/medusa/scrum/SprintResultMatcher.java
@@ -63,6 +63,46 @@ public final class SprintResultMatcher extends TypeSafeMatcher<SprintResult> {
 		return this;
 	}
 
+	public SprintResultMatcher whereDoneCardsContains(Card... card) {
+		addToCombinableMatcher(doneContains(card));
+		return this;
+	}
+
+	public SprintResultMatcher whereDoneCardsDoesNotContain(Card... card) {
+		addToCombinableMatcher(doneDoesNotContain(card));
+		return this;
+	}
+
+	public SprintResultMatcher whereNotDoneCardsContains(Card... card) {
+		addToCombinableMatcher(notDoneContains(card));
+		return this;
+	}
+
+	public SprintResultMatcher whereNotDoneCardsDoesNotContain(Card... card) {
+		addToCombinableMatcher(notDoneDoesNotContain(card));
+		return this;
+	}	
+
+	public SprintResultMatcher whereNotStartedCardsContains(Card... card) {
+		addToCombinableMatcher(notStartedContains(card));
+		return this;
+	}
+
+	public SprintResultMatcher whereNotStartedCardsDoesNotContain(Card... card) {
+		addToCombinableMatcher(notStartedDoesNotContain(card));
+		return this;
+	}
+
+	public SprintResultMatcher whereDefectsCreatedContains(Defect... defect) {
+		addToCombinableMatcher(defectsCreatedContains(defect));
+		return this;
+	}
+
+	public SprintResultMatcher whereDefectsCreatedDoesNotContain(Defect... defect) {
+		addToCombinableMatcher(defectsCreatedDoesNotContain(defect));
+		return this;
+	}
+	
 	public SprintResultMatcher whereDeveloperIdleDaysAre(Integer days) {
 		addToCombinableMatcher(developerIdleDaysAre(days));
 		return this;
@@ -86,12 +126,8 @@ public final class SprintResultMatcher extends TypeSafeMatcher<SprintResult> {
 
 	}
 
-	private Matcher<?> between(Integer low, Integer high) {
-		return both(greaterThan(low)).and(lessThan(high));
-	}
-
 	private Matcher<?> developerIdleDaysBetween(Integer low, Integer high) {
-		return new DeveloperIdleDays((Matcher<? super Integer>) between(low, high), "SprintResult.DeveloperIdleDays",
+		return new DeveloperIdleDays(both(greaterThan(low)).and(lessThan(high)), "SprintResult.DeveloperIdleDays",
 				"DeveloperIdleDays");
 	}
 
@@ -112,6 +148,54 @@ public final class SprintResultMatcher extends TypeSafeMatcher<SprintResult> {
 
 	}
 
+	class Done extends FeatureMatcher<SprintResult, Set<Card>> {
+
+		public Done(Matcher<? super Set<Card>> subMatcher, String featureDescription, String featureName) {
+			super(subMatcher, featureDescription, featureName);
+		}
+
+		@Override
+		protected Set<Card> featureValueOf(SprintResult actual) {
+			return actual.getDone();
+		}
+	}
+
+	class NotDone extends FeatureMatcher<SprintResult, Set<Card>> {
+
+		public NotDone(Matcher<? super Set<Card>> subMatcher, String featureDescription, String featureName) {
+			super(subMatcher, featureDescription, featureName);
+		}
+
+		@Override
+		protected Set<Card> featureValueOf(SprintResult actual) {
+			return actual.getNotDone();
+		}
+	}
+
+	class NotStarted extends FeatureMatcher<SprintResult, Set<Card>> {
+
+		public NotStarted(Matcher<? super Set<Card>> subMatcher, String featureDescription, String featureName) {
+			super(subMatcher, featureDescription, featureName);
+		}
+
+		@Override
+		protected Set<Card> featureValueOf(SprintResult actual) {
+			return actual.getNotStarted();
+		}
+	}
+
+	class DefectsCreated extends FeatureMatcher<SprintResult, Set<Defect>> {
+
+		public DefectsCreated(Matcher<? super Set<Defect>> subMatcher, String featureDescription, String featureName) {
+			super(subMatcher, featureDescription, featureName);
+		}
+
+		@Override
+		protected Set<Defect> featureValueOf(SprintResult actual) {
+			return actual.getDefectsCreated();
+		}
+	}
+
 	private Matcher<?> verifiedDoesNotContain(Card... card) {
 		return new Verified(not(hasItems(card)), "SprintResult.Verified", "Verified");
 	}
@@ -120,6 +204,38 @@ public final class SprintResultMatcher extends TypeSafeMatcher<SprintResult> {
 		return new Verified(hasItems(card), "SprintResult.Verified", "Verified");
 	}
 
+	private Matcher<?> doneDoesNotContain(Card... card) {
+		return new Done(not(hasItems(card)), "SprintResult.Done", "Done");
+	}
+
+	private Matcher<?> doneContains(Card... card) {
+		return new Done(hasItems(card), "SprintResult.Done", "Done");
+	}
+
+	private Matcher<?> notDoneDoesNotContain(Card... card) {
+		return new NotDone(not(hasItems(card)), "SprintResult.NotDone", "NotDone");
+	}
+
+	private Matcher<?> notDoneContains(Card... card) {
+		return new NotDone(hasItems(card), "SprintResult.NotDone", "NotDone");
+	}
+
+	private Matcher<?> notStartedDoesNotContain(Card... card) {
+		return new NotStarted(not(hasItems(card)), "SprintResult.NotStarted", "NotStarted");
+	}
+
+	private Matcher<?> notStartedContains(Card... card) {
+		return new NotStarted(hasItems(card), "SprintResult.NotStarted", "NotStarted");
+	}	
+	
+	private Matcher<?> defectsCreatedDoesNotContain(Defect... defect) {
+		return new DefectsCreated(not(hasItems(defect)), "SprintResult.DefectsCreated", "DefectsCreated");
+	}
+
+	private Matcher<?> defectsCreatedContains(Defect... defect) {
+		return new DefectsCreated(hasItems(defect), "SprintResult.DefectsCreated", "DefectsCreated");
+	}
+	
 	class OriginalSprintData extends FeatureMatcher<SprintResult, String> {
 
 		public OriginalSprintData(Matcher<? super String> matcher, String featureDescription, String featureName) {
@@ -157,6 +273,11 @@ public final class SprintResultMatcher extends TypeSafeMatcher<SprintResult> {
 
 	}
 
+	private SprintResultMatcher hasCards() {
+		addToCombinableMatcher(new CardCount(not(is(0)), "SprintResult...size()", "...size()"));
+		return this;
+	}
+
 	class VerifiedCount extends FeatureMatcher<SprintResult, Integer> {
 
 		public VerifiedCount(Matcher<? super Integer> subMatcher, String featureDescription, String featureName) {
@@ -170,13 +291,81 @@ public final class SprintResultMatcher extends TypeSafeMatcher<SprintResult> {
 
 	}
 
-	private SprintResultMatcher hasCards() {
-		addToCombinableMatcher(new CardCount(not(is(0)), "SprintResult...size()", "...size()"));
-		return this;
+	class DoneCount extends FeatureMatcher<SprintResult, Integer> {
+
+		public DoneCount(Matcher<? super Integer> subMatcher, String featureDescription, String featureName) {
+			super(subMatcher, featureDescription, featureName);
+		}
+
+		@Override
+		protected Integer featureValueOf(SprintResult actual) {
+			return actual.getDone().size();
+		}
+
+	}
+
+	class NotDoneCount extends FeatureMatcher<SprintResult, Integer> {
+
+		public NotDoneCount(Matcher<? super Integer> subMatcher, String featureDescription, String featureName) {
+			super(subMatcher, featureDescription, featureName);
+		}
+
+		@Override
+		protected Integer featureValueOf(SprintResult actual) {
+			return actual.getNotDone().size();
+		}
+
+	}
+
+	class NotStartedCount extends FeatureMatcher<SprintResult, Integer> {
+
+		public NotStartedCount(Matcher<? super Integer> subMatcher, String featureDescription, String featureName) {
+			super(subMatcher, featureDescription, featureName);
+		}
+
+		@Override
+		protected Integer featureValueOf(SprintResult actual) {
+			return actual.getNotStarted().size();
+		}
+
+	}
+
+	class DefectsCreatedCount extends FeatureMatcher<SprintResult, Integer> {
+
+		public DefectsCreatedCount(Matcher<? super Integer> subMatcher, String featureDescription, String featureName) {
+			super(subMatcher, featureDescription, featureName);
+		}
+
+		@Override
+		protected Integer featureValueOf(SprintResult actual) {
+			return actual.getDefectsCreated().size();
+		}
+
 	}
 
 	public SprintResultMatcher hasVerifiedCards() {
 		addToCombinableMatcher(new VerifiedCount(not(is(0)), "SprintResult.Verified.size()", "Verified.size()"));
+		return this;
+	}
+
+	public SprintResultMatcher hasDoneCards() {
+		addToCombinableMatcher(new DoneCount(not(is(0)), "SprintResult.Done.size()", "Done.size()"));
+		return this;
+	}
+
+	public SprintResultMatcher hasNotDoneCards() {
+		addToCombinableMatcher(new NotDoneCount(not(is(0)), "SprintResult.NotDone.size()", "NotDone.size()"));
+		return this;
+	}
+
+	public SprintResultMatcher hasNotStartedCards() {
+		addToCombinableMatcher(new NotStartedCount(not(is(0)), "SprintResult.NotStarted.size()", "NotStarted.size()"));
+		return this;
+	}
+
+	public SprintResultMatcher hasDefectsCreated() {
+		addToCombinableMatcher(new DefectsCreatedCount(not(is(0)), "SprintResult.DefectsCreated.size()",
+				"DefectsCreated.size()"));
 		return this;
 	}
 }
