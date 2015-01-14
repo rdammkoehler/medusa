713d7a8a8509cc43a2d4f8d67757448e33e4838f
Rich Dammkoehler <rpd@noradltd.com>
2015-01-13 21:03:22 -0500

    step files in place

---
+++ src/acceptancetest/java/com/noradltd/medusa/scrum/CapacityPlanningSteps.java
@@ -0,0 +1,35 @@
+package com.noradltd.medusa.scrum;
+
+import cucumber.api.PendingException;
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+
+public class CapacityPlanningSteps {
+
+	@Given("^a developer with (\\d+)% capacity$")
+	public void a_developer_with_capacity(int percentageAvailable) throws Throwable {
+		throw new PendingException();
+	}
+
+	@When("^work assignments occur$")
+	public void work_assignments_occur() throws Throwable {
+		throw new PendingException();
+	}
+
+	@Then("^the developer is assigned work$")
+	public void the_developer_is_assigned_work() throws Throwable {
+		throw new PendingException();
+	}
+
+	@Given("^a developer with unavailable time$")
+	public void a_developer_with_unavailable_time() throws Throwable {
+		throw new PendingException();
+	}
+
+	@Then("^the developer is not assigned work$")
+	public void the_developer_is_not_assigned_work() throws Throwable {
+		throw new PendingException();
+	}
+
+}
---
+++ src/acceptancetest/java/com/noradltd/medusa/scrum/DefectTrackingSteps.java
@@ -0,0 +1,45 @@
+package com.noradltd.medusa.scrum;
+
+import cucumber.api.PendingException;
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+
+public class DefectTrackingSteps {
+	@Given("^a defect occurs in work$")
+	public void a_defect_occurs_in_work() throws Throwable {
+		// Write code here that turns the phrase above into concrete actions
+		throw new PendingException();
+	}
+
+	@When("^the sprint concludes$")
+	public void the_sprint_concludes() throws Throwable {
+		// Write code here that turns the phrase above into concrete actions
+		throw new PendingException();
+	}
+
+	@Then("^I can find the card associated with each defect$")
+	public void i_can_find_the_card_associated_with_each_defect() throws Throwable {
+		// Write code here that turns the phrase above into concrete actions
+		throw new PendingException();
+	}
+
+	@Then("^I can find the developer who created each card$")
+	public void i_can_find_the_developer_who_created_each_card() throws Throwable {
+		// Write code here that turns the phrase above into concrete actions
+		throw new PendingException();
+	}
+
+	@Given("^no defects occur$")
+	public void no_defects_occur() throws Throwable {
+		// Write code here that turns the phrase above into concrete actions
+		throw new PendingException();
+	}
+
+	@Then("^nothing is reported$")
+	public void nothing_is_reported() throws Throwable {
+		// Write code here that turns the phrase above into concrete actions
+		throw new PendingException();
+	}
+
+}
